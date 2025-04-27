f4f18070be43f1b259d1a61b934dd4cc125268ba
zpnever <143313019+zpnever@users.noreply.github.com>
2025-04-27 19:13:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+   preview: {
+    port: 4173,
+    strictPort: true,
+    host: true,
+    allowedHosts: ["inacomp.site", "www.inacomp.site"]
+  }
 })
